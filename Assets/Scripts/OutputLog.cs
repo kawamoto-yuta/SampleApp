6ec9f3541e2dee6afdef5d5f072f67f159caf4f1
k-nakajima365 <nakajima@mediowl.co.jp>
2022-09-05 15:58:44 +0900

    スクリプト名変更

--- Assets/Scripts/OutputLog.cs
+++ Assets/Scripts/OutputLog.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HomeButtonScript : MonoBehaviour
+public class OutputLog : MonoBehaviour
 {
     [SerializeField] Text txt;
     
